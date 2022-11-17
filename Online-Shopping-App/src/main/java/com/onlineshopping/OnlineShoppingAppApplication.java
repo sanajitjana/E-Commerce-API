5b391a17344e9caa19c80d9a0dc6079cad4db6d5
sanajitjana <dev.sanajitjana@gmail.com>
2022-11-17 10:02:34 +0530

    Initial commit

---
+++ Online-Shopping-App/src/main/java/com/onlineshopping/OnlineShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.onlineshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingAppApplication.class, args);
+	}
+
+}
